1ccfc8060575dc4fc30830bc92d6f60486e8050e
Mahesh <mahesh.k7667@gmail.com>
2020-03-02 19:35:48 +0530

    routing

--- Myapp/forms.py
+++ Myapp/forms.py
@@ -5,3 +5,6 @@ class ImgForm(forms.ModelForm):
     class Meta:
         model = ImgModel
         fields = ('img','msg',)
+        widgets = {
+            'msg': forms.Textarea(attrs={'rows': 8, 'cols': 35}),
+        }
